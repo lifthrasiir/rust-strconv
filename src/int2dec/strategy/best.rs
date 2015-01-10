531bc9552ce2ab15a517138809c5bdd94b14d492
Kang Seonghoon <public+git@mearie.org>
2015-01-10 22:48:07 +0900

    it turns out that div100_u32_earlyexit is better for x86_64! updated benchmarks.

--- src/int2dec/strategy/best.rs
+++ src/int2dec/strategy/best.rs
@@ -1,13 +1,13 @@
 #[cfg(test)] use int2dec::testing;
 
-pub use super::bcd_earlyexit::u64_to_digits;
-
+#[cfg(target_arch = "x86")] pub use super::bcd_earlyexit::u64_to_digits;
 #[cfg(target_arch = "x86")] pub use super::div100_earlyexit::u32_to_digits;
-#[cfg(not(target_arch = "x86"))] pub use super::div100::u32_to_digits;
-
-pub use super::div100::u16_to_digits;
-
+#[cfg(target_arch = "x86")] pub use super::div100::u16_to_digits;
 #[cfg(target_arch = "x86")] pub use super::div100_earlyexit::u8_to_digits;
+
+#[cfg(not(target_arch = "x86"))] pub use super::div100_u32_earlyexit::u64_to_digits;
+#[cfg(not(target_arch = "x86"))] pub use super::div100::u32_to_digits;
+#[cfg(not(target_arch = "x86"))] pub use super::div100::u16_to_digits;
 #[cfg(not(target_arch = "x86"))] pub use super::naive::u8_to_digits;
 
 #[cfg(test)] #[bench]
