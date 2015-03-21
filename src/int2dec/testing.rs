423247faf052df57d50be216347b26df71657d0f
Kang Seonghoon <public+git@mearie.org>
2015-03-21 22:06:58 +0900

    language changes: b"xyz" is now of the type `[u8; N]`.

--- src/int2dec/testing.rs
+++ src/int2dec/testing.rs
@@ -7,38 +7,38 @@ use super::digits::{Digits64, Digits32, Digits16, Digits8};
 pub use test::Bencher;
 
 pub fn u64_sanity_test<F: FnMut(u64) -> Digits64>(mut f: F) {
-    assert_eq!(f(                   0), b"00000000000000000000");
-    assert_eq!(f(                   1), b"00000000000000000001");
-    assert_eq!(f(                  12), b"00000000000000000012");
-    assert_eq!(f(                 123), b"00000000000000000123");
-    assert_eq!(f(             1234567), b"00000000000001234567");
-    assert_eq!(f(     123456789012345), b"00000123456789012345");
-    assert_eq!(f(18446744073709551615), b"18446744073709551615");
+    assert_eq!(&f(                   0), b"00000000000000000000");
+    assert_eq!(&f(                   1), b"00000000000000000001");
+    assert_eq!(&f(                  12), b"00000000000000000012");
+    assert_eq!(&f(                 123), b"00000000000000000123");
+    assert_eq!(&f(             1234567), b"00000000000001234567");
+    assert_eq!(&f(     123456789012345), b"00000123456789012345");
+    assert_eq!(&f(18446744073709551615), b"18446744073709551615");
 }
 
 pub fn u32_sanity_test<F: FnMut(u32) -> Digits32>(mut f: F) {
-    assert_eq!(f(         0), b"0000000000");
-    assert_eq!(f(         1), b"0000000001");
-    assert_eq!(f(        12), b"0000000012");
-    assert_eq!(f(       123), b"0000000123");
-    assert_eq!(f(   1234567), b"0001234567");
-    assert_eq!(f(4294967295), b"4294967295");
+    assert_eq!(&f(         0), b"0000000000");
+    assert_eq!(&f(         1), b"0000000001");
+    assert_eq!(&f(        12), b"0000000012");
+    assert_eq!(&f(       123), b"0000000123");
+    assert_eq!(&f(   1234567), b"0001234567");
+    assert_eq!(&f(4294967295), b"4294967295");
 }
 
 pub fn u16_sanity_test<F: FnMut(u16) -> Digits16>(mut f: F) {
-    assert_eq!(f(    0), b"00000");
-    assert_eq!(f(    1), b"00001");
-    assert_eq!(f(   12), b"00012");
-    assert_eq!(f(  123), b"00123");
-    assert_eq!(f(65535), b"65535");
+    assert_eq!(&f(    0), b"00000");
+    assert_eq!(&f(    1), b"00001");
+    assert_eq!(&f(   12), b"00012");
+    assert_eq!(&f(  123), b"00123");
+    assert_eq!(&f(65535), b"65535");
 }
 
 pub fn u8_sanity_test<F: FnMut(u8) -> Digits8>(mut f: F) {
-    assert_eq!(f(  0), b"000");
-    assert_eq!(f(  1), b"001");
-    assert_eq!(f( 12), b"012");
-    assert_eq!(f(123), b"123");
-    assert_eq!(f(255), b"255");
+    assert_eq!(&f(  0), b"000");
+    assert_eq!(&f(  1), b"001");
+    assert_eq!(&f( 12), b"012");
+    assert_eq!(&f(123), b"123");
+    assert_eq!(&f(255), b"255");
 }
 
 #[inline(always)]
