541e81a3caad7282471b8dfc56a3e161fa6577d9
Kang Seonghoon <public+git@mearie.org>
2015-03-21 13:32:33 +0900

    updated flt2dec benchmark (win32 is pending) and added a proper license.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+/* Rust-strconv -- Experimental string-to-number and number-to-string
+ * conversion libraries and benchmarks for Rust.
+ * Written by Kang Seonghoon <http://mearie.org/>.
+ *
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ *
+ *    May you do good and not evil.
+ *    May you find forgiveness for yourself and forgive others.
+ *    May you share freely, never taking more than you give.
+ *
+ * Alternatively, for jurisdictions where authors cannot disclaim
+ * their copyright, this source code is distributed under the terms of
+ * CC0 1.0 Universal license as published by Creative Commons
+ * <https://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * This legal notice and blessing is shamelessly adopted from
+ * the SQLite library.
+ */
+
 #![feature(core, std_misc)] // lib stability features as per RFC #507
 #![cfg_attr(test, feature(libc, test))] // ditto
 
